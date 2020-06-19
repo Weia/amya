2cb03332af04dc3b70c7f838a8de1d3376727ed0
weic <wangweican@cmhi.chinamobile.com>
2020-06-19 16:13:52 +0800

    init

---
+++ src/main/java/com/weic/amya/AmyaApplication.java
@@ -0,0 +1,13 @@
+package com.weic.amya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmyaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmyaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/weic/amya/AmyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weic.amya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmyaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
